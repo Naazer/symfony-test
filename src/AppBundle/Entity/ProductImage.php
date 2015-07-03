72194f3537d3865e46e992d8ef3d7c368c320df0
Nazar Salo <salo@qarea.com>
2015-07-03 13:57:45 +0300

    create product and product image entities

---
+++ src/AppBundle/Entity/ProductImage.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ProductImage
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class ProductImage
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="src", type="string", length=1024)
+     */
+    private $src;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set src
+     *
+     * @param string $src
+     * @return ProductImage
+     */
+    public function setSrc($src)
+    {
+        $this->src = $src;
+
+        return $this;
+    }
+
+    /**
+     * Get src
+     *
+     * @return string 
+     */
+    public function getSrc()
+    {
+        return $this->src;
+    }
+}
